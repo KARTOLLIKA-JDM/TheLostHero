fea693ae4d03fe2000b3a3842f98be850da34f8c
KARTOLLIKA-JDM <61660647+KARTOLLIKA-JDM@users.noreply.github.com>
2022-06-01 17:03:53 +0500

    Создание меню

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,11 +7,15 @@ namespace TheLostHero
 {
     public partial class Form1 : Form
     {
+        private bool isStartGame = false;
         private bool isPressedAnyKey = false;
         private readonly int sizeOfMapObject = 32;
         private readonly Player player;
         private readonly Map map;
         private readonly Bot bot;
+        private readonly Bitmap menu;
+        private Button startButton;
+        private Button exitButton;
 
         public Form1()
         {
@@ -18,6 +23,7 @@ namespace TheLostHero
             player = new Player(new Size(256, 256), 100, 100);
             map = new Map();
             bot = new Bot(false, 0);
+            menu = new Bitmap(@"D:\Игра по C#\Графика\menu.png");
 
             timerForPlayAnimation.Interval = 70;
             timerForPlayAnimation.Tick += new EventHandler(Update);
@@ -27,18 +33,65 @@ namespace TheLostHero
             timerForUpdateMove.Tick += new EventHandler(UpdateMove);
             timerForUpdateMove.Start();
 
+            CreateButton();
+
             KeyDown += new KeyEventHandler(KeyBoard);
             KeyUp += new KeyEventHandler(FreeKey);
 
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            Controls.Add(startButton);
+            Controls.Add(exitButton);
+        }
+
+        private void CreateButton()
+        {
+            startButton = new Button
+            {
+                Location = new Point(660, 300),
+                Size = new Size(235, 55),
+                FlatStyle = FlatStyle.Flat,
+                BackColor = Color.Transparent
+            };
+            startButton.Click += new EventHandler(StartGame);
+            startButton.FlatAppearance.BorderSize = 0;
+            startButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            startButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
+
+            exitButton = new Button
+            {
+                Location = new Point(660, 380),
+                Size = new Size(235, 55),
+                FlatStyle = FlatStyle.Flat,
+                BackColor = Color.Transparent
+            };
+            exitButton.Click += new EventHandler(ExitGame);
+            exitButton.FlatAppearance.BorderSize = 0;
+            exitButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            exitButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
+        }
+
+        private void ExitGame(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            isStartGame = true;
+            Controls.Clear(); // если не почистить кнопки,
+                              // то нажатий на клавиатуру нет
         }
 
         private void Update(object sender, EventArgs e)
         {
-            if (player.currFrame == 3) player.currFrame = 0;
-            player.currFrame++;
-            bot.Move();
-            Invalidate(); // Переотрисовка
+            if (isStartGame)
+            {
+                if (player.currFrame == 3) player.currFrame = 0;
+                player.currFrame++;
+                bot.Move();
+                Invalidate(); // Переотрисовка
+            }
         }
 
         private void FreeKey(object sender, KeyEventArgs e)
@@ -50,6 +103,13 @@ namespace TheLostHero
         private void KeyBoard(object sender, KeyEventArgs e)
         {
             isPressedAnyKey = true;
+            if (e.KeyCode == Keys.Escape) // При нажатии esc выходит в меню
+            {
+                Controls.Add(startButton);
+                Controls.Add(exitButton); // нужно их обратно добавить,
+                                          // иначе их не будет
+                isStartGame = false;
+            }
             CheckStopOrMove(e);
         }
 
@@ -104,22 +164,27 @@ namespace TheLostHero
         {
             Graphics grPaint = e.Graphics;
 
-            map.PresentationMap(grPaint);
-            player.PlayAnimation(grPaint, isPressedAnyKey);
-            bot.PlayAnimation(grPaint);
+            if (isStartGame)
+            {
+                map.PresentationMap(grPaint);
+                player.PlayAnimation(grPaint, isPressedAnyKey);
+                bot.PlayAnimation(grPaint);
+            }
+            else grPaint.DrawImage(menu, new PointF(0, 0));
         }
 
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timerForPlayAnimation = new System.Windows.Forms.Timer(this.components);
-            this.timerForUpdateMove = new System.Windows.Forms.Timer(this.components);
+            this.timerForPlayAnimation = new Timer(components);
+            this.timerForUpdateMove = new Timer(components);
             this.SuspendLayout();
             // 
             // Form1
             // 
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
             this.Location = new Point(0, 0);
-            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.DoubleBuffered = true;
             this.Name = "The Lost Hero";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
